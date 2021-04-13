c90f84a46e59e538b0804c06f2fda816c60092ba
Hans Pelle Jart <hanspellejart@gmail.com>
2021-04-13 21:45:29 +0200

    Scroll into view

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // Global Variables
 const navbarMenu = document.getElementById("navbar__list");
-const sections = document.querySelectorAll("section");
+var sections = document.querySelectorAll("section");
 var navbarItem = document.getElementsByClassName('menu__link')
 
 
@@ -12,16 +12,16 @@ function buildingNav(){
     section.dataset.nav = item.id;
     section.innerText = item.dataset.nav;
     navbarMenu.appendChild(section);
-
-    };};
-    buildingNav();
+    section.addEventListener("click",(e)=>{
+      e.preventDefault();
+      item.scrollIntoView({behavior: "smooth"});
+    })
+  };
+};
+buildingNav();
 
 
 // Add class 'active' to section when near top of viewport
-function togglemenu() {
-  const element = document.getElementById("navbar__list");
-  element.classList.toggle("your-active-class");
-}
 
 
 for (var i = 0; i < navbarItem.length; i++) {
@@ -33,7 +33,17 @@ for (var i = 0; i < navbarItem.length; i++) {
 }
 
 
-window.addEventListener("scroll",function () {
+
+for (var i = 0; i < sections.length; i++) {
+  sections[i].addEventListener("scroll", function() {
+  const current = document.getElementsByClassName("your-active-class");
+  current[0].className = current[0].className.replace(" your-active-class", "");
+  this.className += " your-active-class";
+  });
+}
+
+
+window.addEventListener("scroll", function () {
   if (sections.getBoundingClientRect().top < window.innerHeight) {
      sections.style.background = "#f8f8f8"
   }
@@ -44,16 +54,8 @@ window.addEventListener("scroll",function () {
 
 // Scroll to anchor ID using scrollTO event
 
-sections.addEventListener("click", scroll);
-function scroll() {
-  const sections = document.querySelectorAll("section");
-  sections.scrollIntoView();
-
-}
 
 
-
-  document.getElementById("navbar__list").addEventListener("click", section);
 // Add class 'active' to section when it is near top of viewport
 
 
